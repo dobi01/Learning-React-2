aa38d21b6aaf62f5c6fc68bb994bf866802349fb
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-01-23 21:43:18 +0100

    Fix script

--- script.js
+++ script.js
@@ -18,12 +18,13 @@ var Counter = React.createClass({
     },
 
     componentWillMount() {
-      console.log('Counter A_componentWillMount');
+      console.log(this.props.name + ' componentWillMount');
     },
 
     render: function() {
+      console.log(this.props);
         return React.createElement('div', {},
-            React.createElement('span', {className: 'countA'}, 'counter A'),
+            React.createElement('span', {}, this.props.name),
             React.createElement('div', {className: 'count'}, this.state.counter),
             React.createElement('div', {},
                 React.createElement('button', {onClick: this.increment}, '+1'),
@@ -33,24 +34,24 @@ var Counter = React.createClass({
     },
 
     componentDidMount() {
-        console.log('Counter A_componentDidMount');
+        console.log(this.props.name + ' componentDidMount');
     },
 
     shouldComponentUpdate(nextState) {
         if (this.state.counter !== nextState.counter) {
           let result = true;
-          console.log('Counter A_shouldComponentUpdate: ' + result);
+          console.log(this.props.name + ' shouldComponentUpdate: ' + result);
           return true;
         }
         return false;
     },
 
     componentWillUpdate(nextState) {
-      console.log('Counter A_componentWillUpdate: you can make preparation before an update occurs');
+      console.log(this.props.name + ' componentWillUpdate: you can make preparation before an update occurs');
     },
 
     componentDidUpdate(prevState) {
-      console.log('Counter A_componentWillUpdate: you can operate on the DOM');
+      console.log(this.props.name + ' componentWillUpdate: you can operate on the DOM');
     }
 });
 
@@ -58,14 +59,12 @@ var App = React.createClass({
   render: function() {
     return (
       React.createElement('div', {className: 'app'},
-        React.createElement(Counter, {className: 'CounterA'}),
-        React.createElement(Counter, {className: 'CounterB'}),
+        React.createElement(Counter, {className: 'CounterA', name: 'counter A'}),
+        React.createElement(Counter, {className: 'CounterB', name: 'counter B'})
       )
     );
   }
 });
-
-// var countB = document.getElementByClass('CounterB');
 
 var element = React.createElement(App);
 ReactDOM.render(element, document.getElementById('app'));
